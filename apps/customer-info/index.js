82cded138851bcb5219ac176b359861f913f63ec
Alex Black <alexblack426@gmail.com>
2019-12-16 17:00:12 -0800

    next fixes

--- apps/customer-info/index.js
+++ apps/customer-info/index.js
@@ -16,6 +16,11 @@ export class App {
     this.eId("canContinueBtn").addEventListener("click", () => {
       this.canContinue();
     });
+    this.shellSdk.on(SHELL_EVENTS.Version1.FLOWS.ON_CONTINUE, () => {
+      this.shellSdk.emit(SHELL_EVENTS.Version1.FLOWS.ON_CONTINUE, {
+        output: null
+      });
+    });
   }
   canContinue() {
     this.shellSdk.emit(SHELL_EVENTS.Version1.FLOWS.CAN_CONTINUE, true);
