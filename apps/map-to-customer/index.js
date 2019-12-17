c258d613d687300cc74f10cdbfde3d7c91b3f5f0
spencerk <spencerk@mindtouch.com>
2019-12-16 17:18:47 -0800

    map to customer

---
+++ apps/map-to-customer/index.js
@@ -0,0 +1,29 @@
+import {
+  SHELL_EVENTS,
+  ShellSdk
+} from "/node_modules/fsm-shell/release/fsm-shell-client.es.js";
+
+export class App {
+  static init() {
+    return new App();
+  }
+  constructor() {
+    this.shellSdk = ShellSdk.init(parent, "*");
+    this.shellSdk.emit(SHELL_EVENTS.Version1.FLOWS.REQUIRE_CONTEXT, {
+      clientIdentifier: "map-to-customer-app",
+      clientSecret: "map-to-customer-app-secret"
+    });
+    this.shellSdk.on(SHELL_EVENTS.Version1.FLOWS.ON_CONTINUE, () => {
+      this.shellSdk.emit(SHELL_EVENTS.Version1.FLOWS.ON_CONTINUE, {
+        output: null
+      });
+    });
+    this.canContinue();
+  }
+  canContinue() {
+    this.shellSdk.emit(SHELL_EVENTS.Version1.FLOWS.CAN_CONTINUE, true);
+  }
+}
+
+// Entry point to app
+App.init();
